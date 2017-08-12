bf7f1187cb5e192d6453fa50edd0c3d5b273760e
LironSaedi <lionsaedi@gmail.com>
2017-08-11 17:36:17 -0700

    Initial commit

---
+++ LironSaedi7LED.ino
@@ -0,0 +1,38 @@
+
+int top = 4;
+int topLeft = 7;
+int topRight = 3;
+int middle = 8;
+int bottomRight = 13;
+int bottomLeft = 9;
+int bottom = 12;
+int dot =11;
+void setup()
+{
+  pinMode(top,OUTPUT);
+  pinMode(topLeft,OUTPUT);
+  pinMode(topRight,OUTPUT);
+  pinMode(middle,OUTPUT);
+  pinMode(bottomRight,OUTPUT);
+  pinMode(bottomLeft,OUTPUT);
+  pinMode(bottom,OUTPUT);
+  pinMode(middle,OUTPUT);
+  digitalWrite(top,HIGH);
+digitalWrite(topLeft,HIGH);
+digitalWrite(topRight,HIGH);
+digitalWrite (middle,HIGH);
+digitalWrite(bottomRight,HIGH);
+digitalWrite(bottomLeft,HIGH);
+digitalWrite(bottom,HIGH);
+digitalWrite (dot,HIGH);
+  
+}
+
+
+
+
+void loop()
+{
+
+
+}
