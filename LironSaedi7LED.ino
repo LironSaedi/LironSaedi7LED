5586099baedd3980ed0cb0fb90852a41bd833ed6
LironSaedi <lionsaedi@gmail.com>
2017-08-18 17:39:46 -0700

    Updated

--- LironSaedi7LED.ino
+++ LironSaedi7LED.ino
@@ -1,38 +1,61 @@
 
-int top = 4;
-int topLeft = 7;
-int topRight = 3;
+int topLeft = 2;
+int top = 3;
+int topRight = 4;
+int bottomRight = 5;
+int bottom = 6;
+int bottomLeft = 7;
 int middle = 8;
-int bottomRight = 13;
-int bottomLeft = 9;
-int bottom = 12;
-int dot =11;
+int dot = 9;
+
+int pins[] = { 2, 3, 4, 5, 6, 7, 8, 9 };
+
+int one[] = { 0, 0, 1, 1, 0, 0, 0, 0 };
+int two[] = { 0, 1, 1, 0, 1, 1, 1, 0 };
+int three[] = { 0, 1, 1, 1, 1, 0, 1, 0 };
+
 void setup()
 {
-  pinMode(top,OUTPUT);
-  pinMode(topLeft,OUTPUT);
-  pinMode(topRight,OUTPUT);
-  pinMode(middle,OUTPUT);
-  pinMode(bottomRight,OUTPUT);
-  pinMode(bottomLeft,OUTPUT);
-  pinMode(bottom,OUTPUT);
-  pinMode(middle,OUTPUT);
-  digitalWrite(top,HIGH);
-digitalWrite(topLeft,HIGH);
-digitalWrite(topRight,HIGH);
-digitalWrite (middle,HIGH);
-digitalWrite(bottomRight,HIGH);
-digitalWrite(bottomLeft,HIGH);
-digitalWrite(bottom,HIGH);
-digitalWrite (dot,HIGH);
-  
+  pinMode(top, OUTPUT);
+  pinMode(topLeft, OUTPUT);
+  pinMode(topRight, OUTPUT);
+  pinMode(middle, OUTPUT);
+  pinMode(bottomRight, OUTPUT);
+  pinMode(bottomLeft, OUTPUT);
+  pinMode(bottom, OUTPUT);
+  pinMode(middle, OUTPUT);
 }
-
-
-
 
 void loop()
 {
+  writeNum(one, true);
+  delay(100);
+  writeNum(one, false);
+  delay(100);
+  writeNum(two, true);
+  delay(100);
+  writeNum(two, false);
 
+  delay(100);
 
+  digitalWrite(top, HIGH);
+  digitalWrite(topRight, HIGH);
+  digitalWrite(middle, HIGH);
+  digitalWrite(bottomRight, HIGH);
+  digitalWrite(bottom, HIGH);
+  delay(100);
+  digitalWrite(top, LOW);
+  digitalWrite(topRight, LOW);
+  digitalWrite(middle, LOW);
+  digitalWrite(bottomRight, LOW);
+  digitalWrite(bottom, LOW);
+  delay(100);
+}
+
+void writeNum(int num[], bool on)
+{
+  for (int i = 0; i < 8; i++)
+  {
+    digitalWrite(pins[i], on ? num[i] : 0);
+  }
 }
